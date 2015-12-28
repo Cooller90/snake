74c1cae8fa2ce31450cd6bbe2515d5cdf1423295
Cooller <kfmgtu.kea@gmail.com>
2015-12-28 23:40:50 +0400

    Add field(horizontal and vertical lines)

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xStart, int xEnd,int y, char sym)
+        {
+            pList = new List<Point>();
+
+            for (int x = xStart;x <= xEnd;x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Show()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,11 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            //Point p1 = new Point(1, 3, '*');
+            //p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');           
-            p2.Draw();
+            //Point p2 = new Point(4, 5, '#');           
+            //p2.Draw();
+            Console.SetBufferSize(100, 100);
+
+            HorizontalLine horizontalLine = new HorizontalLine(5, 50, 3, '+');
+            HorizontalLine horizontalLine2 = new HorizontalLine(5, 50, 25, '+');
+            horizontalLine.Show();
+            horizontalLine2.Show();
+
+            Verticalline verticalLine = new Verticalline(3, 25, 4, '+');
+            Verticalline verticalLine2 = new Verticalline(3, 25, 50, '+');
+            verticalLine.Show();
+            verticalLine2.Show();
 
             Console.ReadLine();
         }
